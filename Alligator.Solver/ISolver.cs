aa72bfc5bfcbaa99de7187efeec9fd46d8d35848
Bilics Adrián <bilics.adrian@nexogen.hu>
2023-03-02 22:20:48 +0100

    abstract solver core interfaces

---
+++ Alligator.Solver/ISolver.cs
@@ -0,0 +1,16 @@
+﻿namespace Alligator.Solver
+{
+    /// <summary>
+    /// The main interface of the abstract solver component.
+    /// </summary>
+    /// <typeparam name="TMove">type of moves in the specified game</typeparam>
+    public interface ISolver<TMove>
+    {
+        /// <summary>
+        /// Calculates the optimal next move from the game position defined by move history.
+        /// </summary>
+        /// <param name="moveHistory">ordered list of all previous moves in the current game</param>
+        /// <returns>the optimal next move</returns>
+        TMove OptimizeNextMove(IList<TMove> moveHistory);
+    }
+}
---
+++ Alligator.Solver/SolverProvider.cs
@@ -0,0 +1,33 @@
+﻿namespace Alligator.Solver
+{
+    /// <summary>
+    /// Provides the solver instances.
+    /// </summary>
+    /// <typeparam name="TPosition">type of positions in the specified game</typeparam>
+    /// <typeparam name="TMove">type of moves in the specified game</typeparam>
+    public class SolverProvider<TPosition, TMove>
+    {
+        private readonly IRules<TPosition, TMove> rules;
+        private readonly IConfiguration solverConfiguration;
+        private readonly Action<string> logger;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SolverProvider(IRules<TPosition, TMove> rules, IConfiguration solverConfiguration, Action<string> logger)
+        {
+            this.rules = rules ?? throw new ArgumentNullException(nameof(rules));
+            this.solverConfiguration = solverConfiguration ?? throw new ArgumentNullException(nameof(solverConfiguration));
+            this.logger = logger ?? new Action<string>((logMsg) => { });
+        }
+
+        /// <summary>
+        /// Creates a solver instance.
+        /// </summary>
+        /// <returns>solver instance</returns>
+        public ISolver<TMove> Create()
+        {
+            throw new NotImplementedException("There is no solver implementation yet.");
+        }
+    }
+}
