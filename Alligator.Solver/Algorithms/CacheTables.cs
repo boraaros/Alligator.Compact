6c80d1f6bc5193b09506d130ae54a43bfcb5ce3b
Bilics Adrián <bilics.adrian@nexogen.hu>
2023-03-12 19:41:27 +0100

    simple solver implementation

---
+++ Alligator.Solver/Algorithms/CacheTables.cs
@@ -0,0 +1,27 @@
+﻿namespace Alligator.Solver.Algorithms
+{
+    internal class CacheTables<TPosition, TMove> : ICacheTables<TPosition, TMove>
+    {
+        // TODO: cache implementations!
+
+        public void AddTransposition(TPosition position, Transposition<TMove> transposition)
+        {
+        }
+
+        public void AddValue(TPosition position, int value)
+        {
+        }
+
+        public bool TryGetTransposition(TPosition position, out Transposition<TMove> transposition)
+        {
+            transposition = null;
+            return false;
+        }
+
+        public bool TryGetValue(TPosition position, out int value)
+        {
+            value = 0;
+            return false;
+        }
+    }
+}
